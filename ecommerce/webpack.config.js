28fa846db0b7c6e509ab0f7c5206c0d0bc502433
dhme <dhme@gmail.com>
2026-01-21 03:53:28 +0300

    بدء العمل على مشروع متجر الكتروني + تثبيت نود جي اس وتنصيب مدير الحزم npm + واستخدام اداة webpack وتهيئة تحزيم ملفات html + css +js+ تثبيت السيرفر للتحديث التلقائي

---
+++ ecommerce/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("node:path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+
+module.exports = {
+  mode: "development",  
+  entry: {
+    app: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+  }, 
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    hot: false,
+    open: true,
+    port: 9000,
+    devMiddleware: {
+      writeToDisk: true,
+    }
+  }, 
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          // Disables attributes processing
+          minimize: true,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+        }, 
+        "css-loader"
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+    new CssMinimizerPlugin(),
+  ],
+};
